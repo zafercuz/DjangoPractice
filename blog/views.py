5e4e7af863b5d7803d58b749bf9f741bd6628198
Steven <beltondavid1@gmail.com>
2019-10-30 16:49:41 +0800

    Add view specific user posts
    modified:   .idea/workspace.xml
    modified:   blog/__pycache__/urls.cpython-36.pyc
    modified:   blog/__pycache__/views.cpython-36.pyc
    modified:   blog/templates/blog/home.html
    modified:   blog/templates/blog/post_detail.html
    new file:   blog/templates/blog/user_posts.html
    modified:   blog/urls.py
    modified:   blog/views.py

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 
 
 # Create your views here.
@@ -19,6 +20,17 @@ class PostListView(ListView):
     context_object_name = 'posts'
     ordering = ['-date_posted']
     paginate_by = 5
+
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))  # kwargs = query parameters to be passed
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 
 class PostDetailView(DetailView):
